e73ee0cb2d22e76dffc47c8e88e3249cf56058b5
mydev911 <mydev911@gmail.com>
2023-04-07 22:17:47 -0400

    staticCodeAnalysis

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,6 +1,8 @@
+
 def call(credentialsId) {
-    withSonarQubeEnv(credentialsId: 'credentialsId') {
-    // some block
-    sh 'mvn clean package sonar:sonar'
+    println("Using SonarQube credentials ID: ${credentialsId}")
+    withSonarQubeEnv(credentialsId: credentialsId) {
+        // some block
+        sh 'mvn clean package sonar:sonar'
     }   
 }
